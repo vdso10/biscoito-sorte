dcc942d0e6db4e4d93e47d152e15886855e56904
vdso10 <53046799+vdso10@users.noreply.github.com>
2022-01-05 22:14:46 -0300

    Commit 1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+
+    constructor(props){
+        super(props);
+        this.state = {
+            textoFrase: ''
+        };
+
+        this.quebraBiscoito = this.quebraBiscoito.bind(this);
+
+        this.frases = [
+        'Olhe para cima, que é de lá que vem tua força!',
+        'Você nunca será velho demais para sonhar um novo sonho.',
+        'Siga os bons e aprenda com eles.',
+        'Pensando em desistir? Firme-se nos motivos que te fizeram começar.',
+        'Por mais difícil que algo possa parecer, jamais desista antes de tentar!',
+        'O bom-senso vale mais do que muito conhecimento.', 
+        'O riso é a menor distância entre duas pessoas.', 
+        'Deixe de lado as preocupações e seja feliz.',
+        'Realize o óbvio, pense no improvável e conquiste o impossível.',
+        'Acredite em milagres, mas não dependa deles.',
+        'A maior barreira para o sucesso é o medo do fracasso.',
+        'Agora é a hora de começar a surpreender aqueles que duvidaram de você!',
+        'Valorize as pequenas conquistas.',
+        'Adversidade não é sinônimo de derrota. Vencer os obstáculos é o que nos leva adiante!',
+        'Para certas coisas não basta querer, é preciso batalhar!'
+
+      ];
+
+    }
+
+    quebraBiscoito(){
+        let state = this.state;
+        let numeroAleatorio = Math.floor(Math.random() * this.frases.length);
+        state.textoFrase = '" ' + this.frases[numeroAleatorio] + ' "'
+        this.setState(state);
+
+    }
+
+    render(){
+        return(
+            <div className="container">
+                <img src={require('./assets/biscoito.png')} className="img" alt='biscoito'/>
+                <Botao nome="Abrir biscoito da sorte" acaoBtn={this.quebraBiscoito}/>
+                <h3 className="textoFrase">{this.state.textoFrase}</h3>
+            </div>         
+        );
+    }
 }
+
+class Botao extends Component{
+    render(){
+        return(
+            <div>
+                <button onClick={this.props.acaoBtn} className='btn'>{this.props.nome}</button>
+            </div>
+        );
+    }
+}
+
+
+
 
 export default App;
